c4d7ccd847756b02b33fa6cfa99220c1ea7f8045
karenae <akhilkarenae@gmail.com>
2021-02-11 00:01:00 +0530

    user authentication screens

--- frontend/src/components/screens/Signin.js
+++ frontend/src/components/screens/Signin.js
@@ -1,64 +1,56 @@
-import React,{useState,useContext,} from 'react';
-import {Link,useHistory} from 'react-router-dom';
-import M from 'materialize-css';
-
-
-import {UserContext} from '../../App.js';
+import React,{useState,useContext} from 'react';
+import { Link, useHistory} from 'react-router-dom';
 import './user.css';
-
+import M from 'materialize-css';
+import { UserContext } from '../../App';
 
 const Signin = () => {
-    const {state,dispatch} = useContext(UserContext);
-    const history = useHistory();
-    const [email,setemail] = useState('');
-    const [password,setpassword] = useState('');
 
-    const PostData = ()=>{
+    const {state,dispatch} = useContext(UserContext)
+    const history = useHistory()
+    const [email,setemail] = useState("")
+    const [password,setpassword] = useState("")
+    
+    const userSignin =() =>{
         if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)){
-            M.toast({html:'enter a valid email',classes:'#c62828 red darken-3'})
-            return
+            M.toast({html:"invalid email",classes:"#d50000 red accent-4"})
+            return 
         }
         fetch("http://localhost:4000/api/signin",{
             method:"post",
             headers:{
                 "Content-Type":"application/json",
-                "Authorization":"Bearer"+localStorage.getItem("jwt")
+                // "Authorization":"Bearer "+localStorage.getItem("jwt")
             },
-            body:JSON.stringify({email,password})
+            body:JSON.stringify({
+                email,
+                password})
         }).then(res=>res.json())
         .then(data=>{
             console.log(data)
             if(data.error){
-                M.toast({html: data.error,classes:'#c62828 red darken-3'})
-            }
-            else{
+                M.toast({html:data.error,classes:"#d50000 red accent-4"})
+            }else{
                 localStorage.setItem("jwt",data.token)
-                localStorage.setItem("user",JSON.stringify(data.user))
-                dispatch({type:"USER",payload:data.user})
-                M.toast({html:'sucessfully logedin',classes:'#e0e0e0 grey lighten-2'
-                })
+                localStorage.setItem("saveduser",JSON.stringify(data.saveduser))
+                dispatch({type:"USER",payload:data.saveduser})
+                M.toast({html:"signed in successfully",classes:"#d50000 blue accent-4"})
                 history.push('/')
             }
-        }).catch(err =>{
-            console.log(err)
-        })
+        }).catch(err=>{console.log(err)})
     }
+
     return (
-        <div classname ="signin-card">
+        <div className="mycard">
             <div className="card auth-card input-field">
-                <h2>X-Change</h2>
-                <input type="text" placeholder="email"
-                value={email}
-                onChange={(e)=>setemail(e.target.value)} />
-                <input type="text" placeholder="password" 
-                value={password}
-                onChange={(e)=>setpassword(e.target.value)}/>
-                <a className="waves-effect waves-light btn-large #424242 grey darken-3" onClick={()=>PostData()}>Sign in</a>
-                <br></br>
-                <a>new user? please<Link to="/signup" className='signuplink' >Sign up</Link> here</a>
+                <h3>X-change</h3>
+                <input type="text" placeholder="email"  value={email} onChange={(e)=>setemail(e.target.value)}/>
+                <input type="text" placeholder="password"  value={password} onChange={(e)=>setpassword(e.target.value)}/>
+                <button class="btn waves-effect waves-light" onClick ={(e)=>userSignin()} >Signin</button>
+                <h5><Link to="/signup">Don't have an account?</Link></h5>
             </div>
         </div>
     )
 }
 
-export default Signin;
+export default Signin
--- frontend/src/components/screens/Signup.js
+++ frontend/src/components/screens/Signup.js
@@ -1,59 +1,51 @@
 import React,{useState} from 'react';
-import {Link,useHistory} from 'react-router-dom';
-import M from 'materialize-css';
-
+import { Link, useHistory } from 'react-router-dom';
+import M from "materialize-css";
 import './user.css';
-const Signup = () => {
 
+const Signup = () => {
     const history = useHistory()
     const [name,setname] = useState('')
     const [email,setemail] = useState('')
-    const [password,setpassword] = useState('')
-
-    const PostData = ()=>{
+    const [password,setpassword] = useState("")
+    
+    const userSignup =() =>{
         if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)){
-            M.toast({html:'enter a valid email',classes:'#c62828 red darken-3'})
-            return
+            M.toast({html:"invalid email",classes:"#d50000 red accent-4"})
+            return 
         }
         fetch("http://localhost:4000/api/signup",{
             method:"post",
             headers:{
                 "Content-Type":"application/json"
             },
-            body:JSON.stringify({name,email,password})
+            body:JSON.stringify({
+                name,
+                email,
+                password})
         }).then(res=>res.json())
         .then(data=>{
             if(data.error){
-                M.toast({html: data.error,classes:'#c62828 red darken-3'})
-            }
-            else{
-                M.toast({html:data.message,classes:'#e0e0e0 grey lighten-2'
-                })
+                M.toast({html:data.error,classes:"#d50000 red accent-4"})
+            }else{
+                M.toast({html:data.message,classes:"#d50000 blue accent-4"})
                 history.push('/signin')
             }
-        }).catch(err =>{
-            console.log(err)
-        })
+        }).catch(err=>{console.log(err)})
     }
-    return (
-        <div classname ="signin-card">
-            <div className="card auth-card input-field">
-                <h2>X-Change</h2>
-                <input type="text" placeholder="UserName" value={name}
-                onChange={(e)=>setname(e.target.value)} />
-                <input type="text" placeholder="email"
-                value={email}
-                onChange={(e)=>setemail(e.target.value)} />
-                <input type="text" placeholder="password" 
-                value={password}
-                onChange={(e)=>setpassword(e.target.value)}/>
-                <a className="waves-effect waves-light btn-large #424242 grey darken-3" onClick={()=>PostData()}>Sign up</a>
-                <br />
-                <a>have an account please <Link to="/signin" className = "signinlink">Sign in</Link> here</a>
 
+    return (
+        <div className="mycard">
+            <div className="card auth-card input-field">
+                <h3>X-change</h3>
+                <input type="text" placeholder="name" value={name} onChange={(e)=>setname(e.target.value)} />
+                <input type="text" placeholder="email"  value={email} onChange={(e)=>setemail(e.target.value)}/>
+                <input type="text" placeholder="password"  value={password} onChange={(e)=>setpassword(e.target.value)}/>
+                <button class="btn waves-effect waves-light" onClick ={()=>userSignup()} >Signup</button>
+                <h5><Link to="/signin">Have an account?</Link></h5>
             </div>
         </div>
     )
 }
 
-export default Signup;
+export default Signup
